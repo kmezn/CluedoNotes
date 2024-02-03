24fbc2abea272a41ff6d8514cc1476105a601762
Kmezn <retsek@gmail.com>
2024-02-03 01:09:59 +0000

    Init for Blazor Version

--- CluedoNotes/Data/Card.cs
+++ CluedoNotes/Data/Card.cs
@@ -1,22 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CluedoNotes.Data
+﻿using SQLite;
+namespace CluedoNotes.Data;
+public class Card
 {
-    internal class Card
-    {
-        //[PrimaryKey, AutoIncrement]
-        //public int Id { get; set; }
-        //[MaxLength(255), Unique]
-        //public string Name { get; set; }
-        //public bool IsRoom { get; set; }
-        //public bool IsSuspect {  get; set; }
-        //public bool IsWeapon { get; set; }
-
-        //[OneToMany(CascadeOperations = CascadeOperation.All)]
-        //public List<HeldCard> HeldCards { get; set; }
-    }
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    [MaxLength(255), Unique]
+    public string Name { get; set; }
+    public bool IsRoom { get; set; }
+    public bool IsSuspect { get; set; }
+    public bool IsWeapon { get; set; }
 }
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CluedoNotes.Data
+//{
+//    internal class Card
+//    {
+//        //[PrimaryKey, AutoIncrement]
+//        //public int Id { get; set; }
+//        //[MaxLength(255), Unique]
+//        //public string Name { get; set; }
+//        //public bool IsRoom { get; set; }
+//        //public bool IsSuspect {  get; set; }
+//        //public bool IsWeapon { get; set; }
+
+//        //[OneToMany(CascadeOperations = CascadeOperation.All)]
+//        //public List<HeldCard> HeldCards { get; set; }
+//    }
+//}
