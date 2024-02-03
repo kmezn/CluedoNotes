7d5c608a95c1a81e0a14338fefb523da4341eb18
Kmezn <retsek@gmail.com>
2024-02-03 21:06:56 +0000

    default list of cards for testing.

--- CluedoNotes/Data/DBService.cs
+++ CluedoNotes/Data/DBService.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using Windows.Storage.Streams;
 
 namespace CluedoNotes.Data;
 public class DBService
@@ -17,8 +18,54 @@ public class DBService
             return;
         // Create database and Card Table
         conn = new SQLiteAsyncConnection(_dbPath);
+
         await conn.CreateTableAsync<Card>();
         await conn.CreateTableAsync<Player>();
+
+#if DEBUG
+        var debugCards = new List<Card>()
+        {
+            new Card(){Name ="Miss Scarlett", CardType = CardType.Suspect },
+            new Card(){Name = "Colonel Mustard", CardType = CardType.Suspect },
+            new Card(){Name = "Mrs White", CardType = CardType.Suspect},
+            new Card(){Name = "Reverend Green", CardType = CardType.Suspect },
+            new Card(){Name = "Mrs Peacock", CardType = CardType.Suspect },
+            new Card(){Name = "Professor Plum", CardType = CardType.Suspect },
+            new Card(){Name = "Candlestick", CardType = CardType.Weapon },
+            new Card(){Name = "Dagger", CardType = CardType.Weapon },
+            new Card(){Name = "Lead Piping", CardType = CardType.Weapon },
+            new Card(){Name = "Revolver", CardType = CardType.Weapon },
+            new Card(){Name = "Rope", CardType = CardType.Weapon },
+            new Card(){Name = "Spanner", CardType = CardType.Weapon },
+            new Card(){Name = "Kitchen", CardType = CardType.Room },
+            new Card(){Name = "Ballroom", CardType = CardType.Room },
+            new Card(){Name = "Conservatory", CardType = CardType.Room },
+            new Card(){Name = "Dining Room", CardType = CardType.Room },
+            new Card(){Name = "Billiard Room", CardType = CardType.Room },
+            new Card(){Name = "Library", CardType = CardType.Room },
+            new Card(){Name = "Lounge", CardType = CardType.Room },
+            new Card(){Name = "Hall", CardType = CardType.Room },
+            new Card(){Name = "Study", CardType = CardType.Room },
+        };
+        var debugPlayers = new List<Player>()
+        {
+            new Player(){Name = ""}
+        };
+
+        //debugCards.Select(s => conn.Table<Card>().Where(c => !c.Name.Any(s.Name)).ToListAsync().ForEach(async r => await CreateCardAsync(r));
+        debugPlayers.ForEach(async p => await CreatePlayerAsync(p));
+
+        //List<Task<Card>> cardTasks = new List<Task<Card>>();
+        //debugCards.ForEach(r => cardTasks.Add(CreateCardAsync(r)));
+        //var debugPlayers = new List<Player>()
+        //{
+        //    new Player(){Name = ""}
+        //};
+        //List<Task<Player>> playerTasks = new List<Task<Player>>();
+        //debugPlayers.ForEach(p => playerTasks.Add(CreatePlayerAsync(p)));
+        //Task.WaitAll(playerTasks.ToArray());
+        //Task.WaitAll(cardTasks.ToArray());    
+#endif
     }
     public async Task<List<Card>> GetCardsAsync()
     {
